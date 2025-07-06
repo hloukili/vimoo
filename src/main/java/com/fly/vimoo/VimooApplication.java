3aa312912703a1f5d89ba7f9cb062fdfe4463698
hlo <halim.loukili@rossel.be>
2025-07-06 13:09:15 +0200

    First commit :  initalisation

---
+++ src/main/java/com/fly/vimoo/VimooApplication.java
@@ -0,0 +1,13 @@
+package com.fly.vimoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VimooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fly/vimoo/VimooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fly.vimoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VimooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
